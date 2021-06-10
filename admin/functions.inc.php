57374f68e60540f9a929d4747c3ab989fd3448eb
Mohammed Sibahi <sibahi@live.com>
2021-06-10 04:44:19 +0300

    create the categories page

--- admin/functions.inc.php
+++ admin/functions.inc.php
@@ -1,5 +1,6 @@
 <?php 
 
+ 
 function pr($arr) {
     echo '<pre>';
     print_r($arr);
@@ -9,6 +10,15 @@ function prx($arr) {
     echo '<pre>';
     print_r($arr);
     die();
+}
+
+function get_safe_value ($con, $str) 
+{
+
+    if ($str != " ")
+    // this function is used to create a legal SQL string 
+    // that you can use in an SQL statement
+    return mysqli_real_escape_string($con, $str);
 }
 
 ?>
